9a79ebbb22b23e2ac9308bc15ef04d29ff6ad383
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-11 21:36:56 +0100

    Category buyer controller.

--- app/Http/Controllers/Category/CategoryBuyerController.php
+++ app/Http/Controllers/Category/CategoryBuyerController.php
@@ -10,9 +10,8 @@ use App\Http\Controllers\Controller;
 class CategoryBuyerController extends ApiController
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index(Category $category)
     {

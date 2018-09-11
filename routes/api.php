cb8ed6626d763de8baee62e4054126e2cffeb0da
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-09-11 21:19:03 +0100

    Getting data for category seller biya product.

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show
 
 Route::resource('categories', 'Category\CategoryController', ['except' => ['edit', 'create']]);
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
 
 
 /**

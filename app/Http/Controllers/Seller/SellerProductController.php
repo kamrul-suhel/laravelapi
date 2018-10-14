1747cf04b3d84d67c496cf3c0be517c57fb6242d
Kamrul Ahmed <kamrul_sscis@hotmail.com>
2018-10-14 15:10:42 +0100

    Store seller

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\ApiController;
+use App\Product;
 use App\Seller;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,10 +13,31 @@ class SellerProductController extends ApiController
 {
     /**
      * @param Seller $seller
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index(Seller $seller)
     {
         $products = $seller->products;
         return $this->showAll($products);
+    }
+
+    public function store(Request $request, User $seller){
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        $data['status'] = Product::AVALIABLE_PRODUCT;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+        return $this->showOne($product);
     }
 }
